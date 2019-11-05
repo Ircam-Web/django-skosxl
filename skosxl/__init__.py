9fd28e622e7f235633072b5c71f815e89f50d0d5
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2019-11-05 12:22:25 +0100

    fix app init

--- skosxl/__init__.py
+++ skosxl/__init__.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
+
 VERSION = (0, 3)
 
 def get_version():
     return '%s.%s' % (VERSION[0], VERSION[1])
 
 __version__ = get_version()
+
+default_app_config = 'skosxl.apps.SKOSXLConfig'
